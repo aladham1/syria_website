cb6bb55aa2459f493daac09d3335dd3b2d67a4cf
aladham1 <m.aladham@digitalfuture.sa>
2024-12-09 12:23:01 +0200

    production

--- resources/js/core/Setting.js
+++ resources/js/core/Setting.js
@@ -1,6 +1,6 @@
-window.url_site = 'http://127.0.0.1:8000/manage/';
-window.website_url = 'http://127.0.0.1:8000/';
+//window.url_site = 'http://127.0.0.1:8000/manage/';
+//window.website_url = 'http://127.0.0.1:8000/';
 
-//window.url_site = 'https://syria.website/webapps/manage/';
-//window.website_url = 'https://syria.website/webapps';
+window.url_site = 'https://syria.website/webapps/manage/';
+window.website_url = 'https://syria.website/webapps';
 
